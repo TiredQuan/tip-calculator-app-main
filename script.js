40e98e801fbfe1995cc696d0c0cb0a3ffd674010
TiredQuan <85377764+TiredQuan@users.noreply.github.com>
2021-10-01 15:28:29 +0700

    DONE!

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+let bill=0
 let selectTip=0
+let numPp=0
+let tipAmt=0
+let totalPer=0
+calcTip = () =>{
+    
+    bill=parseFloat(document.getElementById('biller').value)
+    numPp=parseFloat(document.getElementById('numPp').value)
+    tipAmt=bill+(bill/100*selectTip)
+    totalPer=tipAmt/numPp
+    if(Number.isFinite(tipAmt)){
+        document.getElementById('tipAmt').textContent='$'+tipAmt.toFixed(2)
+    }else{
+        document.getElementById('tipAmt').textContent='$0.00'
+    }
+
+    if(Number.isFinite(totalPer)){
+        document.getElementById('totalPp').textContent='$'+totalPer.toFixed(2)
+    }else{
+        document.getElementById('totalPp').textContent='$0.00'
+    }
+}
 tippercent = (x) =>{
     if(x!='custom'){
         selectTip=x
@@ -16,7 +38,47 @@ tippercent = (x) =>{
             tipBtn[i].classList.remove("active")
         }
     }
+    calcTip()
 }
-
-
 tippercent(5)
+
+
+
+billCheck=()=> {
+    let errorBill=document.getElementById('biller')
+    let errorMsg=document.getElementById('billErr')
+    if(errorBill.value==''||Number.parseInt(errorBill.value)==0){
+        errorBill.classList.add('error')
+        errorMsg.style.display="block"
+    } else{
+        errorBill.classList.remove('error')
+        errorMsg.style.display="none"
+    }
+}
+peopleCheck=()=>{
+    let errorPeePee=document.getElementById('numPp')
+    let errorMsg=document.getElementById('error-msg')
+    if(errorPeePee.value==''||Number.parseInt(errorPeePee.value)==0){
+        errorPeePee.classList.add('error')
+        errorMsg.style.display="block"
+    } else{
+        errorPeePee.classList.remove('error')
+        errorMsg.style.display="none"
+    }
+}
+document.addEventListener('keyup',e=()=>{
+    if(document.getElementById('biller').value!=''||document.getElementById('custom').value!=''||document.getElementById('numPp').value!=''){
+        document.getElementById('reset').disabled = false
+    } else {
+        document.getElementById('reset').disabled = true
+    }
+})
+
+resetClick =()=>{
+    document.getElementById('biller').value=''
+    document.getElementById('numPp').value=''
+    document.getElementById('tipAmt').textContent='$0.00'
+    document.getElementById('totalPp').textContent='$0.00'
+    tippercent(5)
+    document.getElementById('reset').disabled = true
+}
